0a328f79ce1fc677c80725cc939c7770c7375548
Guillermo Rojas <guillermo.rojas@financefox.ch>
2016-03-17 17:12:52 +0100

    Changed names to colors.

--- library/src/main/java/com/bruce/pickerview/LoopView.java
+++ library/src/main/java/com/bruce/pickerview/LoopView.java
@@ -73,9 +73,9 @@ public class LoopView extends View {
 
     private void initLoopView(Context context) {
         textSize = 0;
-        colorGray = ActivityCompat.getColor(getContext(), R.color.ff_gray);
-        colorBlack = ActivityCompat.getColor(getContext(), R.color.ff_primary_dark);
-        colorGrayLight = ActivityCompat.getColor(getContext(), R.color.ff_gray);
+        colorGray = ActivityCompat.getColor(getContext(), R.color.picker_grey);
+        colorBlack = ActivityCompat.getColor(getContext(), R.color.picker_primary_dark);
+        colorGrayLight = ActivityCompat.getColor(getContext(), R.color.picker_grey);
         lineSpacingMultiplier = 2.0F;
         isLoop = true;
         initPosition = -1;
